2d317e60af57cc98625bdf5c10e0ebe75b050eae
Han O'Connor <47292779+hanoconnor@users.noreply.github.com>
2023-05-09 20:56:50 +0100

    Star Wars banner added
    Star Wars ASCII banner added for style

--- 4-lists-dictionaries/random-name-generator.py
+++ 4-lists-dictionaries/random-name-generator.py
@@ -12,6 +12,18 @@ noun = ['Millennium Falcon', 'banana', 'Jedi Master']
 random_name = f'{random.choice(first_name)} {random.choice(last_name)}'
 random_sentence = f'{random.choice(first_name)} {random.choice(last_name)} {random.choice(verb)} the {random.choice(noun)}'
 
-# Print random name and random sentence
+# Print banner, random name and random sentence
+print(
+"""
+     _______.___________.    ___      .______         ____    __    ____  ___      .______          _______.
+    /       |           |   /   \     |   _  \        \   \  /  \  /   / /   \     |   _  \        /       |
+   |   (----`---|  |----`  /  ^  \    |  |_)  |        \   \/    \/   / /  ^  \    |  |_)  |      |   (----`
+    \   \       |  |      /  /_\  \   |      /          \            / /  /_\  \   |      /        \   \    
+.----)   |      |  |     /  _____  \  |  |\  \----.      \    /\    / /  _____  \  |  |\  \----.----)   |   
+|_______/       |__|    /__/     \__\ | _| `._____|       \__/  \__/ /__/     \__\ | _| `._____|_______/    
+                                                                                                            
+Name & Sentence Generator v.1 
+"""
+)
 print(f'Your Star Wars name is: {random_name}')
 print(f'A long time ago in a galaxy far, far away {random_sentence}')
